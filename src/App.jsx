973f3d0906b65f98d1a999ca930acce645f152f6
vineet <vineetsn1803.com>
2025-04-04 21:15:35 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { useState,useRef, } from 'react'
+import './App.css'
+
+function App() {
+  
+  const divRef=useRef([]);
+  const divOranage = useRef([]);
+
+  function handleColor(element){ 
+
+    if(divOranage.current.length===0){
+      divOranage.current.push(element);
+      divRef.current[element].style.backgroundColor='green';
+    }
+    else if(divOranage.current.length>0 && divOranage.current.find((ele)=> ele===element)){
+        alert("You have already clicked this element");
+    }
+    else  if(divOranage.current.length>0 && divOranage.current.find((ele)=> ele!==element)){
+       divRef.current[element].style.backgroundColor = "green";
+       divOranage.current.push(element);
+    }
+    
+     if (divOranage.current.length === 9) {
+       divOranage.current.forEach((ele, index) => {
+         setTimeout(() => {
+           divRef.current[ele].style.backgroundColor = "orange";
+         }, index * 500); 
+       });
+
+       setTimeout(() => {
+          divOranage.current = [];
+          divRef.current.forEach((ele) => {
+            ele.style.backgroundColor = "white";
+          });
+       }, divOranage.current.length * 500); 
+    }
+         
+   } 
+
+  return (
+    <>
+      <div className="outer-container">
+        <div className="col">
+          {[1,2,3].map((ele, index) => (
+            <div
+              key={index}
+              className="element"
+              ref={(el) => (divRef.current[ele] = el)}
+              onClick={() => handleColor(ele)}
+            ></div>
+          ))}
+        </div>
+        <div className="col">
+          {[4,5,6].map((ele, index) => (
+            <div
+              key={index}
+              className="element"
+              ref={(el) => (divRef.current[ele] = el)}
+              onClick={() => handleColor(ele)}
+            ></div>
+          ))}
+        </div>
+        <div className="col">
+          {[7,8,9].map((ele, index) => (
+            <div
+              key={index}
+              className="element"
+              ref={(el) => (divRef.current[ele] = el)}
+              onClick={() => handleColor(ele)}
+            ></div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App
